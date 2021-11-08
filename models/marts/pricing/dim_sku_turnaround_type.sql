a6a5cb5c71d29243e61b32829bb457f31f929060
bruno.guerreiro <bruno.guerreiro@helloprint.com>
2021-11-08 14:10:13 +0100

    dim_turnaround_type

---
+++ models/marts/pricing/dim_sku_turnaround_type.sql
@@ -0,0 +1,37 @@
+WITH turnaround_fastest AS (
+  SELECT
+    date_price_updated,
+    country_name,
+    LOWER(product_name) AS product_name,
+    sku,
+    LEFT(sku, LENGTH(sku) - instr(REVERSE(sku), '-', 1, 1)) AS sku_no_turnaround,
+    'fastest' AS turnaround_type
+  FROM
+    {{ ref('stg_bigquery-data-analytics__pricing_monitoring_2') }}
+),
+turnaround_slowest AS (
+  SELECT
+    date_price_updated,
+    country_name,
+    LOWER(product_name) AS product_name,
+    sku,
+    LEFT(sku, LENGTH(sku) - instr(REVERSE(sku), '-', 1, 1)) AS sku_no_turnaround,
+    'slowest' AS turnaround_type
+  FROM
+    {{ ref('stg_bigquery-data-analytics__pricing_monitoring_2') }}
+),
+union_data AS (
+  SELECT
+    *
+  FROM
+    turnaround_fastest
+  UNION ALL
+  SELECT
+    *
+  FROM
+    turnaround_slowest
+)
+SELECT
+  *
+FROM
+  union_data
