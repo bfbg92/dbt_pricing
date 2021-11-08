12a229b8e41ec28a7ccdd2b4689464ec62c0f645
bruno.guerreiro <bruno.guerreiro@helloprint.com>
2021-11-08 14:32:19 +0100

    bugfix

--- models/marts/pricing/dim_sku_turnaround_type.sql
+++ models/marts/pricing/dim_sku_turnaround_type.sql
@@ -4,7 +4,6 @@ WITH turnaround_fastest AS (
     country_name,
     company_name,
     LOWER(product_name) AS product_name,
-    CONCAT(LEFT(sku, LENGTH(sku) - instr(REVERSE(sku), '-', 1, 1)),'-',MIN(turnaround)) AS sku,
     LEFT(sku, LENGTH(sku) - instr(REVERSE(sku), '-', 1, 1)) AS sku_no_turnaround,
     'fastest' AS turnaround_type,
     MIN(turnaround) AS turnaround
@@ -18,22 +17,22 @@ turnaround_slowest AS (
     country_name,
     company_name,
     LOWER(product_name) AS product_name,
-    CONCAT(LEFT(sku, LENGTH(sku) - instr(REVERSE(sku), '-', 1, 1)),'-',MIN(turnaround)) AS sku,
     LEFT(sku, LENGTH(sku) - instr(REVERSE(sku), '-', 1, 1)) AS sku_no_turnaround,
     'slowest' AS turnaround_type,
     MAX(turnaround) AS turnaround
   FROM
     {{ ref('stg_bigquery-data-analytics__pricing_monitoring_2') }}
   GROUP BY 1,2,3,4,5,6
+
 ),
 union_data AS (
   SELECT
-    *
+    *, CONCAT(sku_no_turnaround,'-',turnaround) AS sku
   FROM
     turnaround_fastest
   UNION ALL
   SELECT
-    *
+    *, CONCAT(sku_no_turnaround,'-',turnaround) AS sku
   FROM
     turnaround_slowest
 )
