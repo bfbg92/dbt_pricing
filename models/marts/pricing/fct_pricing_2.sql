9851c7e21bb3153eb77595339666a602b1516520
bruno.guerreiro <bruno.guerreiro@helloprint.com>
2021-11-09 14:28:40 +0100

    refactor

--- models/marts/pricing/fct_pricing_2.sql
+++ models/marts/pricing/fct_pricing_2.sql
@@ -1,13 +1,13 @@
 {{ config(materialized='table') }}
 
-WITH fill_nulls_temp as (
+WITH 
+inner_join_turnaround_type AS (
 SELECT 
   fact.date_price_updated,
   fact.country_name,
   fact.product_name,
   fact.sku,
   dim.sku_no_turnaround,
-  fact.company_name,
   fact.quantity,
   fact.turnaround,
   dim.turnaround_type,
@@ -16,20 +16,15 @@ SELECT
   fact.cover,
   fact.finishing,
 -- helloprint,
-   SUM(CASE WHEN fact.company_name = 'Helloprint' THEN fact.price ELSE NULL END) OVER (PARTITION BY fact.country_name, fact.product_name, fact.sku ORDER BY fact.date_price_updated ASC) as price_helloprint,
-   SUM(CASE WHEN fact.company_name = 'Helloprint' AND fact.price IS NULL THEN 0 ELSE 1 END) OVER (PARTITION BY fact.country_name, fact.product_name, fact.sku ORDER BY fact.date_price_updated ASC) as helloprint_partition,
+   SUM(CASE WHEN fact.company_name = 'Helloprint' THEN fact.price ELSE NULL END) as price_helloprint,
 -- helloprint_connect,
-   SUM(CASE WHEN fact.company_name = 'Helloprint Connect' THEN fact.price ELSE NULL END) OVER (PARTITION BY fact.country_name, fact.product_name, fact.sku ORDER BY fact.date_price_updated ASC) as price_helloprint_connect,
-   SUM(CASE WHEN fact.company_name = 'Helloprint Connect' AND fact.price IS NULL THEN 0 ELSE 1 END) OVER (PARTITION BY fact.country_name, fact.product_name, fact.sku ORDER BY fact.date_price_updated ASC) as helloprint_connect_partition,
+   SUM(CASE WHEN fact.company_name = 'Helloprint Connect' THEN fact.price ELSE NULL END) as price_helloprint_connect,
 -- printoclock,
-   SUM(CASE WHEN fact.company_name = 'printoclock' THEN fact.price ELSE NULL END) OVER (PARTITION BY fact.country_name, fact.product_name, fact.sku ORDER BY fact.date_price_updated ASC) as price_printoclock,
-   SUM(CASE WHEN fact.company_name = 'printoclock' AND fact.price IS NULL THEN 0 ELSE 1 END) OVER (PARTITION BY fact.country_name, fact.product_name, fact.sku ORDER BY fact.date_price_updated ASC) as printoclock_partition,
+   SUM(CASE WHEN fact.company_name = 'printoclock' THEN fact.price ELSE NULL END) as price_printoclock,
 -- realisaprint
-   SUM(CASE WHEN fact.company_name = 'realisaprint' THEN fact.price ELSE NULL END) OVER (PARTITION BY fact.country_name, fact.product_name, fact.sku ORDER BY fact.date_price_updated ASC) as price_realisaprint,
-   SUM(CASE WHEN fact.company_name = 'realisaprint' AND fact.price IS NULL THEN 0 ELSE 1 END) OVER (PARTITION BY fact.country_name, fact.product_name, fact.sku ORDER BY fact.date_price_updated ASC) as realisaprint_partition,
+   SUM(CASE WHEN fact.company_name = 'realisaprint' THEN fact.price ELSE NULL END) as price_realisaprint,
 -- flyeralarm
-   SUM(CASE WHEN fact.company_name = 'flyeralarm' THEN fact.price ELSE NULL END) OVER (PARTITION BY fact.country_name, fact.product_name, fact.sku ORDER BY fact.date_price_updated ASC) as price_flyeralarm,
-   SUM(CASE WHEN fact.company_name = 'flyeralarm' AND fact.price IS NULL THEN 0 ELSE 1 END) OVER (PARTITION BY fact.country_name, fact.product_name, fact.sku ORDER BY fact.date_price_updated ASC) as flyeralarm_partition
+   SUM(CASE WHEN fact.company_name = 'flyeralarm' THEN fact.price ELSE NULL END) as price_flyeralarm
 FROM {{ ref('stg_bigquery-data-analytics__pricing_monitoring_2') }} fact
 INNER JOIN {{ ref('dim_sku_turnaround_type') }} dim ON 
   fact.date_price_updated = dim.date_price_updated AND
@@ -37,7 +32,39 @@ INNER JOIN {{ ref('dim_sku_turnaround_type') }} dim ON
   fact.product_name = dim.product_name AND
   fact.sku = dim.sku AND
   fact.company_name = dim.company_name
-ORDER BY fact.sku ASC, fact.date_price_updated ASC),
+GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12),
+
+fill_nulls_temp as (
+SELECT 
+  date_price_updated,
+  country_name,
+  product_name,
+  sku,
+  sku_no_turnaround,
+  company_name,
+  quantity,
+  turnaround,
+  turnaround_type,
+  material,
+  size,
+  cover,
+  finishing,
+-- helloprint,
+   price_helloprint,
+   SUM(CASE WHEN price_helloprint IS NULL THEN 0 ELSE 1 END) OVER (PARTITION BY country_name, sku_no_turnaround, turnaround_type ORDER BY date_price_updated ASC) as helloprint_partition,
+-- helloprint_connect,
+   price_helloprint_connect,
+   SUM(CASE WHEN price_helloprint_connect IS NULL THEN 0 ELSE 1 END) OVER (PARTITION BY country_name, sku_no_turnaround, turnaround_type ORDER BY date_price_updated ASC) as helloprint_connect_partition,
+-- printoclock,
+   price_printoclock,
+   SUM(CASE WHEN price_printoclock IS NULL THEN 0 ELSE 1 END) OVER (PARTITION BY country_name, sku_no_turnaround, turnaround_type ORDER BY date_price_updated ASC) as printoclock_partition,
+-- realisaprint
+   price_realisaprint,
+   SUM(CASE WHEN price_realisaprint IS NULL THEN 0 ELSE 1 END) OVER (PARTITION BY country_name, sku_no_turnaround, turnaround_type ORDER BY date_price_updated ASC) as realisaprint_partition,
+-- flyeralarm
+   price_flyeralarm,
+   SUM(CASE WHEN price_flyeralarm IS NULL THEN 0 ELSE 1 END) OVER (PARTITION BY country_name, sku_no_turnaround, turnaround_type ORDER BY date_price_updated ASC) as flyeralarm_partition
+FROM inner_join_turnaround_type),
 -----------------------------------------------------------------------------------------------------
 
 -- At this step, null values of competitor price columns are filled with previous non-null price.
@@ -57,17 +84,16 @@ size,
 cover,
 finishing,
 -- helloprint,
-FIRST_VALUE(price_helloprint) OVER (PARTITION BY sku, helloprint_partition ORDER BY date_price_updated) as price_helloprint,
+FIRST_VALUE(price_helloprint) OVER (PARTITION BY country_name, sku_no_turnaround, turnaround_type, helloprint_partition ORDER BY date_price_updated ASC) as price_helloprint,
 -- helloprint_connect,
-FIRST_VALUE(price_helloprint_connect) OVER (PARTITION BY sku, helloprint_connect_partition ORDER BY date_price_updated) as price_helloprint_connect,
+FIRST_VALUE(price_helloprint_connect) OVER (PARTITION BY country_name, sku_no_turnaround, turnaround_type, helloprint_connect_partition ORDER BY date_price_updated) as price_helloprint_connect,
 -- printoclock,
-FIRST_VALUE(price_printoclock) OVER (PARTITION BY sku, printoclock_partition ORDER BY date_price_updated) as price_printoclock,
+FIRST_VALUE(price_printoclock) OVER (PARTITION BY country_name, sku_no_turnaround, turnaround_type, printoclock_partition ORDER BY date_price_updated) as price_printoclock,
 -- realisaprint
-FIRST_VALUE(price_realisaprint) OVER (PARTITION BY sku, realisaprint_partition ORDER BY date_price_updated) as price_realisaprint,
+FIRST_VALUE(price_realisaprint) OVER (PARTITION BY country_name, sku_no_turnaround, turnaround_type, realisaprint_partition ORDER BY date_price_updated) as price_realisaprint,
 -- flyeralarm
-FIRST_VALUE(price_flyeralarm) OVER (PARTITION BY sku, flyeralarm_partition ORDER BY date_price_updated) as price_flyeralarm,
-FROM fill_nulls_temp
-ORDER BY SKU ASC, date_price_updated ASC),
+FIRST_VALUE(price_flyeralarm) OVER (PARTITION BY country_name, sku_no_turnaround, turnaround_type, flyeralarm_partition ORDER BY date_price_updated) as price_flyeralarm,
+FROM fill_nulls_temp),
 -----------------------------------------------------------------------------------------------------
 
 -- At this step, previous competitor price columns are generated (price_lag).
@@ -88,19 +114,19 @@ cover,
 finishing,
 -- helloprint
 price_helloprint,
-LAG(price_helloprint, 1) OVER (PARTITION BY sku ORDER BY date_price_updated  ASC) AS price_lag_helloprint,
+LAG(price_helloprint, 1) OVER (PARTITION BY country_name, sku_no_turnaround, turnaround_type ORDER BY date_price_updated ASC) AS price_lag_helloprint,
 -- helloprint_connect
 price_helloprint_connect,
-LAG(price_helloprint_connect, 1) OVER (PARTITION BY sku ORDER BY date_price_updated  ASC) AS price_lag_helloprint_connect,
+LAG(price_helloprint_connect, 1) OVER (PARTITION BY country_name, sku_no_turnaround, turnaround_type ORDER BY date_price_updated ASC) AS price_lag_helloprint_connect,
 -- printoclock
 price_printoclock,
-LAG(price_printoclock, 1) OVER (PARTITION BY sku ORDER BY date_price_updated  ASC) AS price_lag_printoclock,
+LAG(price_printoclock, 1) OVER (PARTITION BY country_name, sku_no_turnaround, turnaround_type ORDER BY date_price_updated ASC) AS price_lag_printoclock,
 -- realisaprint
 price_realisaprint,
-LAG(price_realisaprint, 1) OVER (PARTITION BY sku ORDER BY date_price_updated  ASC) AS price_lag_realisaprint,
+LAG(price_realisaprint, 1) OVER (PARTITION BY country_name, sku_no_turnaround, turnaround_type ORDER BY date_price_updated ASC) AS price_lag_realisaprint,
 -- flyeralarm
 price_flyeralarm,
-LAG(price_flyeralarm, 1) OVER (PARTITION BY sku ORDER BY date_price_updated  ASC) AS price_lag_flyeralarm
+LAG(price_flyeralarm, 1) OVER (PARTITION BY country_name, sku_no_turnaround, turnaround_type ORDER BY date_price_updated ASC) AS price_lag_flyeralarm
 FROM fill_nulls)
 -----------------------------------------------------------------------------------------------------
 
