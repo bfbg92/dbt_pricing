3ee905e827cb71712ad3ff67883cb6992164eafb
bruno.guerreiro <bruno.guerreiro@helloprint.com>
2021-11-11 11:21:00 +0100

    oi

--- models/marts/pricing/fct_pricing.sql
+++ models/marts/pricing/fct_pricing.sql
@@ -57,13 +57,10 @@ SELECT
   cost_price,
   supplier_price,
   carrier_cost,
-
   {% for company in companies %}
-  (
      price_{{ company }},
      price_{{ company }},
      SUM(CASE WHEN price_{{ company }} IS NULL THEN 0 ELSE 1 END) OVER (PARTITION BY country_name, sku_no_turnaround, turnaround_type ORDER BY date_price_updated ASC) as {{ company }}_partition,
-  )
   {% endfor %}
 FROM join_turnaround_type),
 -----------------------------------------------------------------------------------------------------
