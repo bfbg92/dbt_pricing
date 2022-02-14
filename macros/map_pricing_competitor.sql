efc6d2c682ef4e856033ec7ad226f707a3fee896
Bruno Guerreiro <bruno.guerreiro@helloprint.com>
2022-02-14 12:46:07 +0100

    trying

--- macros/map_pricing_competitor.sql
+++ macros/map_pricing_competitor.sql
@@ -1,14 +1,17 @@
 {% macro map_pricing_competitor(competitor)%}
 
-    {% set competitor_dict = var('pricing_competitor_map_dict') %}
-    {% set result = competitor %}
-    
-	{% for k, v in competitor_dict.items() %}
-        {% for i in v %}
-            {% if i == competitor %}
-                {% set result = k %}
-		{% endfor %}
-    {% endfor %}
-    {% return(result) %}
+{% set competitor_dict = var('pricing_competitor_map_dict') %}
+{% set result = competitor %}
+
+{% for k, v in competitor_dict.items() %}
+  {% for i in v %}
+    {% if i == competitor %}
+    {% set result = k %}
+    {% endif %}
+  {% endfor %}
+{% endfor %}
+
+{{ log("result: " ~ result, True) }}
+{{ return(result) }}
     
 {% endmacro %}
