6d67cfba039e2d63bbc297cbaa67903128e1e70a
Bruno Guerreiro <bruno.guerreiro@helloprint.com>
2022-02-21 13:55:25 +0100

    tuf

--- macros/pricing/map_competitor.sql
+++ macros/pricing/map_competitor.sql
@@ -1,11 +1,11 @@
 {% macro map_comp(competitor)%}
 {% set competitor_dict = var('pricing_competitor_map_dict') %}
 CASE
-{% for k, v in competitor_dict.items() -%}
-  {% for i in v -%}
-  WHEN '{{ i }}' = '{{ competitor }}' THEN '{{ k }}'
+{% for k, v in competitor_dict.items() %}
+  {%- for i in v -%}
+  WHEN '{{ i }}' = {{ competitor }} THEN '{{ k }}'
   {% endfor -%}
 {% endfor -%}
-ELSE '{{ competitor }}'
+ELSE {{ competitor }}
 END
 {% endmacro %}
