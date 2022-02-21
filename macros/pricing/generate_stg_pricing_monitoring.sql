fd3e2e3f73eebf48b41d05372384be92ba91c6ce
Bruno Guerreiro <bruno.guerreiro@helloprint.com>
2022-02-21 12:01:27 +0100

    abc

--- macros/pricing/generate_stg_pricing_monitoring.sql
+++ macros/pricing/generate_stg_pricing_monitoring.sql
@@ -36,7 +36,7 @@ filtered_renamed AS (
     AND (
       (salesprice_comp_r1 IS NOT NULL AND salesprice_comp_r1 > 0)
       OR
-      (salesprice_comp_all IS NOT NULL AND salesprice_comp_all > 0)
+      (salesprice_comp_all IS NOT NULL AND salesprice_comp_all > 0))
     GROUP BY 1,2,3,4,5,6,7,8,9,10
     )
 
