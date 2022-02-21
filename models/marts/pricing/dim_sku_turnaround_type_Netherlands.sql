3fc385682e8c817cdee5ae72bee11ae97af7b3dd
Bruno Guerreiro <bruno.guerreiro@helloprint.com>
2022-02-21 15:36:06 +0100

    fact tables

--- models/marts/pricing/dim_sku_turnaround_type_Netherlands.sql
+++ models/marts/pricing/dim_sku_turnaround_type_Netherlands.sql
@@ -1,9 +1,9 @@
 /* input parameters */
 {% set competitors = var('pricing_competitors') %}
 
-{% set france_competitors = namespace(value=['']) %}
-{% for k, v in competitors.items() if k == 'France' %}
-    {% set france_competitors.value = v %}
+{% set netherlands_competitors = namespace(value=['']) %}
+{% for k, v in competitors.items() if k == 'Netherlands' %}
+    {% set netherlands_competitors.value = v %}
 {% endfor %}
 
-{{ generate_dim_sku_turnaround_type('France') }} 
+{{ generate_dim_sku_turnaround_type('Netherlands') }} 
--- models/marts/pricing/dim_sku_turnaround_type_United_Kingdom.sql
+++ models/marts/pricing/dim_sku_turnaround_type_United_Kingdom.sql
@@ -1,9 +1,9 @@
 /* input parameters */
 {% set competitors = var('pricing_competitors') %}
 
-{% set france_competitors = namespace(value=['']) %}
-{% for k, v in competitors.items() if k == 'France' %}
-    {% set france_competitors.value = v %}
+{% set united_kingdom_competitors = namespace(value=['']) %}
+{% for k, v in competitors.items() if k == 'United_Kingdom' %}
+    {% set united_kingdom_competitors.value = v %}
 {% endfor %}
 
-{{ generate_dim_sku_turnaround_type('France') }} 
+{{ generate_dim_sku_turnaround_type('United_Kingdom') }} 
