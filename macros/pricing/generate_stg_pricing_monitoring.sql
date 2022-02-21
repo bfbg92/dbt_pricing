98d0af896caa9ca8decbb80fabdae2514503278b
Bruno Guerreiro <bruno.guerreiro@helloprint.com>
2022-02-21 13:11:30 +0100

    last_test_

--- macros/pricing/generate_stg_pricing_monitoring.sql
+++ macros/pricing/generate_stg_pricing_monitoring.sql
@@ -1,8 +1,9 @@
-{% macro generate_stg_pricing_monitoring(country, competitors, products) %}
+{% macro generate_stg_pricing_monitoring(country, competitors, competitors_mapped, products) %}
 
 
 WITH 
-filtered_renamed AS (
+
+map_filter_rename AS (
   SELECT
     CAST(time_price_updated AS DATE) AS spider_update_at,
     LOWER(product_name) AS product_name,
@@ -13,34 +14,51 @@ filtered_renamed AS (
     size,
     cover,
     finishing,
+    {{ map_comp(competitor_name) }} AS competitor_renamed,
+    competitor_price_comp_r1,
+    salesprice_comp_r1 AS price_helloprint,
+    salesprice_comp_all AS price_helloprint_connect,
+    cost_price,
+    supplier_price,
+    carrier_cost
+  FROM {{ source('bigquery-data-analytics', 'pricing_monitoring') }}
+  WHERE
+    country_name = '{{ country }}' AND
+    product_name IN ('{{ "','".join(products) }}') AND
+    competitor_renamed IN ('{{ "','".join(competitors) }}') AND (
+      (salesprice_comp_r1 IS NOT NULL AND salesprice_comp_r1 > 0) OR
+      (salesprice_comp_all IS NOT NULL AND salesprice_comp_all > 0))),
+
+grouped AS (
+  SELECT
+    spider_update_at,
+    product_name,
+    sku,
+    quantity,
+    turnaround,
+    material,
+    size,
+    cover,
+    finishing,
     /* loop through competitors */
-    {% for competitor in competitors -%}
+    {% for competitor in competitors_mapped -%}
     MAX(
       if(
-        competitor_name = '{{ competitor }}'
+        competitor_renamed = '{{ competitor }}'
         AND competitor_price_comp_r1 > 0,
         competitor_price_comp_r1,
         NULL)
     ) AS price_{{ competitor }},
     {% endfor -%}
-    MAX(salesprice_comp_r1) AS price_helloprint,
-    MAX(salesprice_comp_all) AS price_helloprint_connect,
+    MAX(price_helloprint) AS price_helloprint,
+    MAX(price_helloprint_connect) AS price_helloprint_connect,
     MAX(cost_price) AS cost_price,
     MAX(supplier_price) AS supplier_price,
     MAX(carrier_cost) AS carrier_cost
-    FROM {{ source('bigquery-data-analytics', 'pricing_monitoring') }}
-    WHERE
-    country_name = '{{ country }}'
-    AND product_name IN ('{{ "','".join(products) }}')
-    AND competitor_name IN ('{{ "','".join(competitors) }}')
-    AND (
-      (salesprice_comp_r1 IS NOT NULL AND salesprice_comp_r1 > 0)
-      OR
-      (salesprice_comp_all IS NOT NULL AND salesprice_comp_all > 0))
-    GROUP BY 1,2,3,4,5,6,7,8,9
-    )
+    FROM map_filter_rename
+    GROUP BY 1,2,3,4,5,6,7,8,9)
 
-SELECT * FROM filtered_renamed 
+SELECT * FROM grouped 
 
 
 {% endmacro %}
--- models/staging/bigquery-data-analytics/stg_bigquery-data-analytics__pricing_monitoring_France.sql
+++ models/staging/bigquery-data-analytics/stg_bigquery-data-analytics__pricing_monitoring_France.sql
@@ -1,10 +1,16 @@
 /* input parameters */
 {% set competitors = var('pricing_competitors') %}
-{% set products = var('pricing_products') %}
-
 {% set france_competitors = namespace(value=['']) %}
 {% for k, v in competitors.items() if k == 'France' %}
     {% set france_competitors.value = v %}
 {% endfor %}
 
-{{ generate_stg_pricing_monitoring('France', france_competitors.value, products) }} 
+{% set pricing_competitors_mapped = var('pricing_competitors_mapped') %}
+{% set france_competitors_mapped = namespace(value=['']) %}
+{% for k, v in pricing_competitors_mapped.items() if k == 'France' %}
+    {% set france_competitors_mapped.value = v %}
+{% endfor %}
+
+{% set products = var('pricing_products') %}
+
+{{ generate_stg_pricing_monitoring('France', france_competitors.value, france_competitors_mapped.value, products) }} 
