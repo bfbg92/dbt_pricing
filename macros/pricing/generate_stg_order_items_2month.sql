f3ed57b9b21387cbe98b44479f1b8749a4c88e02
Bruno Guerreiro <bruno.guerreiro@helloprint.com>
2022-03-28 15:19:25 +0200

    generate sum princing

---
+++ macros/pricing/generate_stg_order_items_2month.sql
@@ -0,0 +1,14 @@
+{% macro generate_stg_order_items_2month(country) %}
+
+  SELECT
+    sku_product_identifier,
+    sku_material,
+    sku_size,
+    sku_printing_option,
+    sku_finishing,
+    total_revenue,
+    order_items
+  FROM {{ source('bigquery-data-analytics', 'order_items_2month') }}
+  WHERE country  = '{{ country }}'
+
+{% endmacro %}
